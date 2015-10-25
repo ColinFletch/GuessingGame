9edfd727a701f8949c017e397adefb675afbdc9e
ColinFletch <cfletch@us.ibm.com>
2015-10-25 18:52:14 -0400

    First COmmit

---
+++ src/com/colinsOrg/guessGame/GameLauncher.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.colinsOrg.guessGame;
+
+/**
+ * @author cfletch
+ *
+ */
+public class GameLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+
+}
